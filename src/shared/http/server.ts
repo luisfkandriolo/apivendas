296b05dd65e7291fcfcc0d08db76180c2b9d92b0
Luis F K Andriolo <luisfandriolo@gmail.com>
2024-02-19 18:25:46 -0300

    a

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,11 +7,14 @@ import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 import customErrorMessage from '@utils/customErrorMessage';
+import upload from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(upload.directory));
 
 app.use(routes);
 
